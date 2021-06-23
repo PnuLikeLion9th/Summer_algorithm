dbafc5435634caea9d8dae83794cb9171c6e1623
Hoovely <kimwooa23@naver.com>
2021-06-24 03:54:09 +0900

    RGB거리2

--- donghae/직사각형 별찍기.py
+++
@@ -1,5 +0,0 @@
-a, b = map(int, input().strip().split(' '))
-for i in range(b):	#세로 길이만큼 반복
-    for j in range(a):	#가로 길이만큼 별찍기
-        print('*', end='')
-    print()	#줄바꿈`
--- dongyeop/직사각형 별찍기.py
+++
@@ -1,3 +0,0 @@
-a, b = map(int, input().strip().split(' '))
-for i in range(b):#세로의 길이만큼 반복
-    print(a*'*')#가로의 길이만큼 별을 찍음
