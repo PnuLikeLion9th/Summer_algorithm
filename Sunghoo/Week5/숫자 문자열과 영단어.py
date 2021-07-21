8a05769b926a3651768cab468573f3c8a8dac963
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-07-22 01:04:44 +0900

    선택 1/3

--- Sunghoo/Week5/숫자 문자열과 영단어.py
+++ Sunghoo/Week5/숫자 문자열과 영단어.py
@@ -32,6 +32,13 @@ def solution(s):
 
     return int(''.join(answer))
 
-
+num_dic = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
+           "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+def solution2(s):
+    answer = s
+    for key, value in num_dic.items():
+        print(answer.replace(key, value))
+        # answer = answer.replace(key, value)
+    # return int(answer)
 s = "2three45sixseven"
-print(solution(s))
+print(solution2(s))
