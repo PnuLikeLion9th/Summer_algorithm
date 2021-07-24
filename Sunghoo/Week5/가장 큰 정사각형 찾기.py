a4ef24986af43092fa5fec853d549607c20d2f84
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-07-24 23:14:07 +0900

    선택 3/3

---
+++ Sunghoo/Week5/가장 큰 정사각형 찾기.py
@@ -0,0 +1,40 @@
+def check(dr, dc, N, M):
+    if 0 <= dr <= N and 0 <= dc <= M:
+        return True
+
+
+def solution(board):
+    max_val = 0
+    for row in board:
+        max_val += sum(row)
+    if max_val == 0:
+        return 0
+    else:
+        max_val = 1
+
+    N = len(board) - 1
+    M = len(board[0]) - 1
+    indexes = [(idx, jdx) for jdx in range(len(board[0])) for idx in range(len(board))]
+
+    for r, c in indexes:
+        if board[r][c] and check(r + 1, c + 1, N, M):
+            dr = r + 1
+            dc = c + 1
+            if board[dr][dc] and board[dr][c] and board[r][dc]:
+                board[dr][dc] = min(board[r][c], board[dr][c], board[r][dc]) + 1
+                if max_val < board[dr][dc]:
+                    max_val = board[dr][dc]
+    return max_val ** 2
+
+
+# 모범 풀이
+def solution(board):
+    answer = 0
+    for i in range(1, len(board)) :
+        for j in range(1, len(board[0])) :
+            if board[i][j] >= 1 :
+                board[i][j] += min(board[i-1][j-1],board[i][j-1],board[i-1][j])
+
+    for i in board :
+        if answer < max(i) : answer = max(i)
+    return answer * answer
