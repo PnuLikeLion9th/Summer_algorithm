8daaecfe5be0186bfa5ec0acd3306f76fade1891
밍아리 <tothemoon_andback@naver.com>
2021-07-31 15:30:21 +0900

    6주차

---
+++ mina/10886.py
@@ -0,0 +1,13 @@
+#백준 10886
+
+V = int(input())
+cute = 0
+
+for _ in range(V):
+    if int(input()) == 1:
+        cute += 1
+
+if cute > V//2:
+    print("Junhee is cute!")
+else:
+    print("Junhee is not cute!")
